e125f1a979197652e975a46cca89709de8c9b315
Aleksandra Chełmińska <aichelminska99@gmail.com>
2026-03-10 22:04:36 +0100

    remove node_modules and .bundle from tracking

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,9 +3,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import { Platform } from 'react-native';
 
 import LoginScreen from '../screens/LoginScreen';
-import DashboardScreen from '../screens/DashboardScreen';
+import DashboardScreen from '../screens/HomeScreen';
 import SearchScreen from '../screens/SearchScreen';
 import MyVaultScreen from '../screens/MyVaultScreen';
 import WishlistScreen from '../screens/WishlistScreen';
@@ -17,39 +18,48 @@ const Tab = createBottomTabNavigator();
 function TabNavigator() {
   return (
     <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: 'black',
-          borderTopWidth: 0,
-        },
-        tabBarActiveTintColor: 'white',
-        tabBarInactiveTintColor: 'gray',
-      }}>
+    screenOptions={{
+      headerShown: false,
+      tabBarStyle: {
+        backgroundColor: 'black',
+        borderTopWidth: 0,
+        height: 80,
+        paddingTop: 5,
+        paddingBottom: Platform.OS === 'android' ? 0 : 10,
+      },
+      tabBarIconStyle: {
+        marginBottom: 5,
+      },
+      tabBarLabelStyle: {
+        fontSize: 12,
+      },
+      tabBarActiveTintColor: 'white',
+      tabBarInactiveTintColor: 'gray',
+    }}>
       <Tab.Screen
-        name="Featured"
+        name="Home"
         component={DashboardScreen}
-        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="home" size={24} color={color} /> }}
+        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="star" size={28} color={color} /> }}
       />
       <Tab.Screen
         name="Search"
         component={SearchScreen}
-        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="search1" size={24} color={color} /> }}
+        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="search1" size={28} color={color} /> }}
       />
       <Tab.Screen
         name="My Vault"
         component={MyVaultScreen}
-        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="playcircleo" size={24} color={color} /> }}
+        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="playcircleo" size={28} color={color} /> }}
       />
       <Tab.Screen
         name="Wishlist"
         component={WishlistScreen}
-        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="hearto" size={24} color={color} /> }}
+        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="hearto" size={28} color={color} /> }}
       />
       <Tab.Screen
         name="Account"
         component={AccountScreen}
-        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="user" size={24} color={color} /> }}
+        options={{ tabBarIcon: ({ color }) => <AntDesignIcon name="user" size={28} color={color} /> }}
       />
     </Tab.Navigator>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, Text, StyleSheet } from 'react-native';
 
-function HomeScreen({ navigation }: any): React.JSX.Element {
-
-    const handleLogout = async () => {
-        await AsyncStorage.removeItem('token');
-        navigation.replace('Login');
-      };
-
+export default function DashboardScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Witaj w DigiVault! 🎉</Text>
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonText} onPress={handleLogout}>Wyloguj</Text>
-      </TouchableOpacity>
+      <Text>Dashboard</Text>
     </View>
   );
 }
@@ -24,26 +14,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'black',
-  },
-  title: {
-    color: 'white',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 40,
-  },
-  button: {
-    padding: 15,
-    backgroundColor: '#E4E4E4',
-    borderRadius: 20,
-    width: 200,
-  },
-  buttonText: {
-    color: 'black',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 18,
   },
 });
-
-export default HomeScreen;
